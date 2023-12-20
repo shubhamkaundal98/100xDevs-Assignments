0dd3c2e61e29428f7d0b8cacfc2c3e83859f7e56
shubhamkaundal98 <shubham.kaundal98@gmail.com>
2023-12-20 18:51:16 +0530

    middlewares assignments completed

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -16,6 +16,24 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+function limit(req,res,next) {
+  const userId = req.headers['user-id']
+
+  if(!numberOfRequestsForUser[userId]){
+    numberOfRequestsForUser[userId] =1
+  } else {
+    numberOfRequestsForUser[userId] += 1
+
+    if(numberOfRequestsForUser[userId]>5){
+      res.status(404).send()
+    }
+  }
+
+  next()
+}
+
+app.use(limit)
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
